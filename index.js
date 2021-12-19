fca6c7fbad78d2a5d1a6f7f3b19cd1a4b3b62bb4
WinPCi9 <saldoz@ya.ru>
2021-12-19 03:44:57 +0300

    Add: добавлен post-запрос на главную страницу и обработка JSON-а

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 import express from 'express'
 
+//Номер порта
 const PORT = 5000;
 
+// Создаем объект Express-а
 const app = express()
 
-app.get('/', (req, res) => {
-  res.status(200).json('Сервер работает')
+// Для того, чтобы Express понимал JSON-формат, отправляемый ему от пользователя в BODY
+app.use(express.json())
+
+// Эндпоинт / - главная страница
+app.post('/', (req, res) => {
+  console.log(req.params)
+  console.log(req.path)
+  console.log(req.baseUrl)
+  console.log(req.body)
+  console.log(req.hostname)
+  console.log(req.ip)
+  console.log(req.protocol)
+  console.log(req.query)
+  console.log(req.originalUrl)
+  console.log(req.method)
+  console.log(req.mode)
+  res.status(200).json('Сервер работает3333')
 })
 
 app.listen(PORT, () => console.log('SERVER STARTED AT ' + PORT))
