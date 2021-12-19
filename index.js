611d0c9b71cf8b4d30d2b89bbfabf5b8054dfba5
WinPCi9 <saldoz@ya.ru>
2021-12-19 04:11:04 +0300

    Add: index.js: добавлена возможность сохранять статью в базу данных, если отправить POST-запрос на главную страницу: {     "author": "John",     "title": "Тестовая статья",     "content": "Текст статьи" }

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import Post from "./Post.js";
 
 // URL подключения к БД:
 const DB_URL = 'mongodb+srv://user:user@cluster0.lgikb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
@@ -14,19 +15,13 @@ const app = express()
 app.use(express.json())
 
 // Эндпоинт / - главная страница
-app.post('/', (req, res) => {
-  console.log(req.params)
-  console.log(req.path)
-  console.log(req.baseUrl)
-  console.log(req.body)
-  console.log(req.hostname)
-  console.log(req.ip)
-  console.log(req.protocol)
-  console.log(req.query)
-  console.log(req.originalUrl)
-  console.log(req.method)
-  console.log(req.mode)
-  res.status(200).json('Сервер работает3333')
+app.post('/', async (req, res) => {
+  // Распаковываем боди
+  const {author, title, content, picture} = req.body
+  // Создаем запись в БД
+  const post = await Post.create({author, title, content, picture})
+  // Возвращаем нужный статус
+  res.status(201).json({post})
 })
 
 async function startApp() {
